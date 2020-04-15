24180a5032148ea35a6a58d4bbfcdf5616a33d9e
Kindamba <kindambaliungani@gmail.com>
2020-04-15 15:37:04 +0300

    Update sw.js
    In swahili we say "siku 3 zilizopita" not masiku 3 zilizopita   " dd : 'siku %d' "

--- locale/sw.js
+++ locale/sw.js
@@ -41,7 +41,7 @@
             h : 'saa limoja',
             hh : 'masaa %d',
             d : 'siku moja',
-            dd : 'masiku %d',
+            dd : 'siku %d',
             M : 'mwezi mmoja',
             MM : 'miezi %d',
             y : 'mwaka mmoja',
